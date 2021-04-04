74320227de90489f9336b75f37cd8589de894a5b
darhubat <79753845+darhubat@users.noreply.github.com>
2021-04-04 10:55:57 +0200

    Zweites Histogramm erstellt für die Darstellung der Ausgaben pro Ausgaben-Thema. Beim ersten Histogramm noch einen interaktiven Slider erstellt, um die Zeitraum der Anzeige variabel zu gestalten.

--- Projekt_App_Ausgabenkontrolle/Datenvisualisierung/chart.py
+++ Projekt_App_Ausgabenkontrolle/Datenvisualisierung/chart.py
@@ -9,6 +9,7 @@ def data_viz():
     return data
 
 
+""""
 def viz_barchart():
     data = data_viz()
     fig = px.bar(data, x='Monat', y='Betrag', hover_data=['typ', 'Jahr', 'Monat', 'Betrag', 'Thema'], color='typ',
@@ -16,7 +17,9 @@ def viz_barchart():
     fig.update_xaxes(dtick=1, range=[1, 12])
     fig.update_layout(xaxis_title='Monate', yaxis_title='Betrag in CHF')
     div = fig.show()
+    #  div = plot(fig, output_type='div')
     return div
+"""
 
 
 def viz_histogram():
@@ -27,5 +30,46 @@ def viz_histogram():
     fig.update_traces(xbins_size="M1")
     fig.update_xaxes(ticklabelmode='period', dtick='M1', tickformat='%b\n%Y', showspikes=True)
     fig.update_yaxes(showspikes=True)
+    fig.update_layout(
+        xaxis=dict(
+            rangeselector=dict(
+                buttons=list([
+                    dict(count=1,
+                         label="1m",
+                         step="month",
+                         stepmode="backward"),
+                    dict(count=6,
+                         label="6m",
+                         step="month",
+                         stepmode="backward"),
+                    dict(count=1,
+                         label="YTD",
+                         step="year",
+                         stepmode="todate"),
+                    dict(count=1,
+                         label="1y",
+                         step="year",
+                         stepmode="backward"),
+                    dict(step="all")
+                ])
+            ),
+            rangeslider=dict(
+                visible=True
+            ),
+            type="date"
+        )
+    )
     div_1 = fig.show()
     return div_1
+
+
+def viz_histogram_thema():
+    data = daten_ausgabeneingabe()
+    fig = px.histogram(data, x='Jahr', y='Betrag', hover_name='typ', hover_data=['Thema'], color='Thema',
+                        title='Ausgaben nach Thema', barmode='group')
+    fig.update_layout(xaxis_title='Jahre', yaxis_title='Betrag in CHF', bargap=0.2, hovermode='x')
+    fig.update_traces(xbins_size="M1")
+    fig.update_xaxes(ticklabelmode='period', showspikes=True)
+    fig.update_yaxes(showspikes=True)
+    div_2 = fig.show()
+    return div_2
--- Projekt_App_Ausgabenkontrolle/Datenvisualisierung/main.py
+++ Projekt_App_Ausgabenkontrolle/Datenvisualisierung/main.py
@@ -2,15 +2,19 @@ from flask import Flask
 from flask import render_template
 import plotly.express as px
 from Projekt_App_Ausgabenkontrolle.Datenvisualisierung.datenvorbereitung.datengrundlage import daten_mergen
+from Projekt_App_Ausgabenkontrolle.Datenvisualisierung.datenvorbereitung.datengrundlage import daten_ausgabeneingabe
 from plotly.offline import plot
 
 
 app = Flask("Datenvisualisierung")
 
+
 def data_viz():
     data = daten_mergen()
     return data
 
+
+""""
 def viz_barchart():
     data = data_viz()
     fig = px.bar(data, x='Monat', y='Betrag', hover_data=['typ', 'Jahr', 'Monat', 'Betrag', 'Thema'], color='typ',
@@ -20,6 +24,8 @@ def viz_barchart():
     div = fig.show()
     #  div = plot(fig, output_type='div')
     return div
+"""
+
 
 def viz_histogram():
     data = data_viz()
@@ -29,18 +35,60 @@ def viz_histogram():
     fig.update_traces(xbins_size="M1")
     fig.update_xaxes(ticklabelmode='period', dtick='M1', tickformat='%b\n%Y', showspikes=True)
     fig.update_yaxes(showspikes=True)
+    fig.update_layout(
+        xaxis=dict(
+            rangeselector=dict(
+                buttons=list([
+                    dict(count=1,
+                         label="1m",
+                         step="month",
+                         stepmode="backward"),
+                    dict(count=6,
+                         label="6m",
+                         step="month",
+                         stepmode="backward"),
+                    dict(count=1,
+                         label="YTD",
+                         step="year",
+                         stepmode="todate"),
+                    dict(count=1,
+                         label="1y",
+                         step="year",
+                         stepmode="backward"),
+                    dict(step="all")
+                ])
+            ),
+            rangeslider=dict(
+                visible=True
+            ),
+            type="date"
+        )
+    )
     div_1 = fig.show()
     return div_1
 
 
+def viz_histogram_thema():
+    data = daten_ausgabeneingabe()
+    fig = px.histogram(data, x='Jahr', y='Betrag', hover_name='typ', hover_data=['Thema'], color='Thema',
+                        title='Ausgaben nach Thema', barmode='group')
+    fig.update_layout(xaxis_title='Jahre', yaxis_title='Betrag in CHF', bargap=0.2, hovermode='x')
+    fig.update_traces(xbins_size="M1")
+    fig.update_xaxes(ticklabelmode='period', showspikes=True)
+    fig.update_yaxes(showspikes=True)
+    div_2 = fig.show()
+    return div_2
+
+
 @app.route("/viz")
 def index():
-    div = viz_barchart()
+    div = viz_histogram()
     return render_template('index.html', viz_div=div)
+
 
 @app.route("/viz2")
 def index_2():
-    div_1 = viz_histogram()
+    div_1 = viz_histogram_thema()
     return render_template('index.html', viz_div=div_1)
 
 
